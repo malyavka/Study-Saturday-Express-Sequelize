8ff1f538b71eefd2857e1e1baeca83acbf6153a4
Maria Obaturova <m.obaturova@gmail.com>
2019-04-27 14:41:37 -0400

    without PUT

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const init = async () => {
     }
   }
 
-}
+};
 
 init();
 
--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,62 @@
 const router = require('express').Router();
+//const db = require('../db');
 
+const Student = require('../db/models/student');
+
+router.get('/', async(req, res, next) => {
+    try {
+        const students = await Student.findAll();
+        res.send(students);
+    } catch (err) {
+        next(err);
+    }
+
+});
+
+router.get('/:id', async (req, res, next) => {
+    //console.log(req.params.id);
+    try {
+        const student = await Student.findById(req.params.id);
+        if (student) {
+            res.send(student);
+        } else {
+            res.status(404).send('Not found');
+        }
+    }
+    catch (err) {
+        next(err);
+    }
+
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newStudent = await Student.create(req.body);
+        res.status(201).send(newStudent);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.put('/:id', async (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await Student.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(204).send('Deleted');
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports = router;
